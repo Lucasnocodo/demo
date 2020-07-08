17134e83986b385826c33776cca41b783dea6516
Lucas Chen <h51301.c@gmail.com>
2020-07-08 23:55:38 +0800

    depoly

---
+++ precache-manifest.af6b693d55ab50e12ddf891838c7b24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e9a6616b1ce046475b5d949f2a56fa",
+    "url": "/frontend_YT_test/index.html"
+  },
+  {
+    "revision": "8a2a7df0129dca88589c",
+    "url": "/frontend_YT_test/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "49e0206cac47049a4b7c",
+    "url": "/frontend_YT_test/static/css/main.eea4d19a.chunk.css"
+  },
+  {
+    "revision": "8a2a7df0129dca88589c",
+    "url": "/frontend_YT_test/static/js/2.3e839f0f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/frontend_YT_test/static/js/2.3e839f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e0206cac47049a4b7c",
+    "url": "/frontend_YT_test/static/js/main.0036ce5f.chunk.js"
+  },
+  {
+    "revision": "ad16db0f19e5c9f3d6bd",
+    "url": "/frontend_YT_test/static/js/runtime-main.c0673cac.js"
+  }
+]);
---
+++ precache-manifest.c5797642eadaea34a2eef2566aede02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b335835050cea7b1e6431104b53f3a0",
+    "url": "frontend_YT_test/index.html"
+  },
+  {
+    "revision": "e8ddd3037befadc70c2a",
+    "url": "frontend_YT_test/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "512cc7593a865a4bca86",
+    "url": "frontend_YT_test/static/css/main.eea4d19a.chunk.css"
+  },
+  {
+    "revision": "e8ddd3037befadc70c2a",
+    "url": "frontend_YT_test/static/js/2.577e7fc5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "frontend_YT_test/static/js/2.577e7fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512cc7593a865a4bca86",
+    "url": "frontend_YT_test/static/js/main.17d63ab1.chunk.js"
+  },
+  {
+    "revision": "1799bdba94d92cfffa0b",
+    "url": "frontend_YT_test/static/js/runtime-main.a0a98ebf.js"
+  }
+]);
